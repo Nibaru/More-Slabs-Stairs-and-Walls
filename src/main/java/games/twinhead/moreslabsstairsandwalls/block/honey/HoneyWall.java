7676fa86823707f4a7e41d95dd6c632907e71b8c
user <stratecide@gmail.com>
2023-10-29 10:52:48 +0100

    make honey-walls behave more like honey-blocks

--- src/main/java/games/twinhead/moreslabsstairsandwalls/block/honey/HoneyWall.java
+++ src/main/java/games/twinhead/moreslabsstairsandwalls/block/honey/HoneyWall.java
@@ -2,10 +2,15 @@ package games.twinhead.moreslabsstairsandwalls.block.honey;
 
 import games.twinhead.moreslabsstairsandwalls.block.base.BaseWall;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
@@ -16,40 +21,40 @@ public class HoneyWall extends BaseWall {
     }
 
     public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
-        if(!world.isClient())
-            if (entity.bypassesLandingEffects()) {
-                super.onLandedUpon(world, state, pos, entity, fallDistance);
-            } else {
-                entity.handleFallDamage(fallDistance, 0.0F, world.getDamageSources().fall());
-            }
-
-    }
-
-    public void onEntityLand(BlockView world, Entity entity) {
-            if (entity.bypassesLandingEffects()) {
-                super.onEntityLand(world, entity);
-            } else {
-                this.bounce(entity);
-            }
-
-    }
-
-    private void bounce(Entity entity) {
-        Vec3d vec3d = entity.getVelocity();
-        if (vec3d.y < 0.0) {
-            double d = entity instanceof LivingEntity ? 1.0 : 0.8;
-            entity.setVelocity(vec3d.x, -vec3d.y * d, vec3d.z);
+        entity.playSound(SoundEvents.BLOCK_HONEY_BLOCK_SLIDE, 1.0f, 1.0f);
+        if (!world.isClient) {
+            world.sendEntityStatus(entity, EntityStatuses.DRIP_RICH_HONEY);
         }
-
+        if (entity.handleFallDamage(fallDistance, 0.2f, world.getDamageSources().fall())) {
+            entity.playSound(this.soundGroup.getFallSound(), this.soundGroup.getVolume() * 0.5f, this.soundGroup.getPitch() * 0.75f);
+        }
     }
 
-    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        double d = Math.abs(entity.getVelocity().y);
-        if (d < 0.1 && !entity.bypassesSteppingEffects()) {
-            double e = 0.4 + d * 0.2;
-            entity.setVelocity(entity.getVelocity().multiply(e, 1.0, e));
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (isSliding(pos, entity)) {
+            HoneySlab.triggerAdvancement(entity, pos);
+            HoneySlab.updateSlidingVelocity(entity);
+            HoneySlab.addCollisionEffects(world, entity);
         }
+        super.onEntityCollision(state, world, pos, entity);
+    }
 
-        super.onSteppedOn(world, pos, state, entity);
+    private boolean isSliding(BlockPos pos, Entity entity) {
+        if (entity.isOnGround()) {
+            return false;
+        }
+        if (entity.getY() > (double)pos.getY() + 1.4375 - 1.0E-7) {
+            return false;
+        }
+        if (entity.getVelocity().y >= -0.08) {
+            return false;
+        }
+        VoxelShape shape = this.getCollisionShape(entity.getWorld().getBlockState(pos), entity.getWorld(), pos, ShapeContext.absent());
+        double entityRadius = entity.getWidth() / 2.0f;
+        return entity.getX() - entityRadius + 1.0E-7 > shape.getMax(Direction.Axis.X) ||
+                entity.getZ() - entityRadius + 1.0E-7 > shape.getMax(Direction.Axis.Z) ||
+                entity.getX() + entityRadius - 1.0E-7 < shape.getMin(Direction.Axis.X) ||
+                entity.getZ() + entityRadius - 1.0E-7 < shape.getMin(Direction.Axis.Z);
     }
 }
